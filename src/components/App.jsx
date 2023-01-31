c76e8338293a879ac8997158f29ec4c5c669b86f
Ravinder aggarwal <aggarwalravinder3301@gmail.com>
2023-01-31 18:35:48 +0530

    made todolist

---
+++ src/components/App.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import List from "./list";
+function App() {
+  const [contentit, addcont] = useState("");
+  const [array, addarray] = useState([]);
+  function addcontent(event) {
+    const content = event.target.value;
+    console.log(content);
+    addcont(content);
+  }
+  function dothis() {
+    const middle = contentit;
+    addcont("");
+    addarray((previos) => {
+      return [...previos, middle];
+    });
+  }
+
+  return (
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <div className="form">
+        <input type="text" onChange={addcontent} value={contentit} />
+        <button onClick={dothis}>
+          <span>Add</span>
+        </button>
+      </div>
+      <div>
+        <ul>
+          {array.map((contact) => {
+            return <List name={contact} />;
+          })}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/list.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+function List(prop) {
+  return <li>{prop.name}</li>;
+}
+export default List;
